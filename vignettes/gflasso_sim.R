8f1075fcaacdeb79a7889c95d126e62bd00a51fd
Kris Sankaran <sankaran.kris@gmail.com>
2016-04-27 19:14:38 -0700

    finalize vignette for gflasso

--- vignettes/gflasso_sim.R
+++ vignettes/gflasso_sim.R
@@ -6,8 +6,11 @@
 ## ---- sparse-sim-packages ----
 # List of packages for session
 .packages = c("gflasso",
+              "plyr",
+              "dplyr",
               "reshape2",
               "glmnet",
+              "Rcpp",
               "ggplot2")
 
 # Install CRAN packages (if not already installed)
@@ -24,20 +27,23 @@ cat("\014")  # Clear console
 
 rm(list=ls()) # Delete all existing variables
 graphics.off() # Close all open plots
+theme_set(theme_bw())
+sim_methods_dir <- "~/Documents/programming/simFuns/simMethods/"
 
 ## ---- simdata-params ----
-K <- 50 # number of columns of Y
-J <- 100 # number of columns of X
-J0 <- 20 # number of columns of X actually contribute
-n <- 200 # number of samples
+K <- 200 # number of columns of Y
+J <- 30 # number of columns of X
+J0 <- 5 # number of columns of X actually contribute
+n <- 40 # number of samples
 
 ## ---- gflasso-simulate-data ----
-beta <- rnorm(J)
-B <- beta %*% t(rep(1, K)) + matrix(rnorm(J * K, 0, .2), J, K)
+beta <- rnorm(J, .5, 1)
+beta[sample(J)[1:(J / 2)]] <- rnorm(J / 2, -.5, 1)
+B <- beta %*% t(rep(1, K)) + matrix(rnorm(J * K, 0, .1), J, K)
 B[-sample(J, J0), 1 : (K / 2)] <- 0
 B[-sample(J, J0), (K / 2 + 1) : K] <- 0
 X <- matrix(rnorm(n * J), n, J)
-Y <- X %*% B + matrix(rnorm(n * K), n, K)
+Y <- X %*% B + matrix(rnorm(n * K, 0, .5), n, K)
 
 ## ---- vis-B ----
 m_b <- melt(B, varnames = c("feature", "task"), value.name = "beta")
@@ -53,18 +59,18 @@ reg_data <- m_y %>%
   left_join(m_x) %>%
   left_join(m_b)
 cur_data <- reg_data %>%
-  filter(task %in% 20:30,
-         feature %in% 10:18)
+  filter(task %in% 95:105,
+         feature %in% 1:10)
 
 ## ---- vis-reg-lines ----
 ggplot(cur_data) +
-  geom_point(aes(x = x, y = y, col = as.factor(beta == 0)), 
+  geom_point(aes(x = x, y = y, col = beta == 0),
              size = .3, alpha = 0.3) +
   geom_abline(data = cur_data %>% filter(beta != 0),
               aes(slope = beta, intercept = 0, col = as.factor(beta == 0))) +
   scale_color_manual(values = c("#8068ab", "#d9bad8")) +
   facet_grid(feature ~ task) +
-  theme(panel.grid = element_blank(),
+ theme(panel.grid = element_blank(),
         panel.border = element_blank(),
         panel.margin = unit(0, "lines"),
         strip.background = element_blank(),
@@ -75,10 +81,17 @@ ggplot(cur_data) +
 R <- matrix(1, K, K)
 R[(K / 2 + 1) : K, 1:(K / 2)] <- 0
 R[1:(K / 2), (K / 2 + 1) : K] <- 0
-gflasso_res <- gflasso(Y, X, R, list(delta_conv = 1e-10, lambda = 8, gamma = 2, iter_max = 1200, eps = .1))
+n_iter <- 200
+
+lambda <- .15
+gamma <- .05
+eps <- .1
+gflasso_res <- gflasso(Y, X, R,
+                       list(delta_conv = 1, lambda = lambda, gamma = gamma,
+                            iter_max = n_iter, verbose = T, eps = eps))
 
 ## ---- vis-objective ----
-obj_data <- data.frame(iteration = seq_len(1200),
+obj_data <- data.frame(iteration = seq_along(gflasso_res$obj),
                        objective = gflasso_res$obj)
 ggplot(obj_data) +
   geom_line(aes(x = iteration, y = objective)) +
@@ -100,7 +113,6 @@ ggplot(b_compare) +
 ## ---- vis-Bhat-gflasso ----
 gflasso_mb <- melt(gflasso_res$B, varnames = c("feature", "task"),
                value.name = "beta_hat")
-gflasso_mb$small <- gflasso_mb$beta_hat < 5e-2
 
 ggplot(gflasso_mb) +
   geom_tile(aes(x = task, y = feature, fill = beta_hat)) +
@@ -114,8 +126,8 @@ reg_fit_data <- m_y %>%
   left_join(gflasso_mb)
 reg_fit_data$zero <- reg_fit_data$beta == 0
 cur_fit_data <- reg_fit_data %>%
-  filter(task %in% 20:30,
-         feature %in% 10:18) %>%
+  filter(task %in% 95:105,
+         feature %in% 1:10) %>%
   melt(measure.vars = c("beta", "beta_hat"), variable.name = "coef")
 
 ## ---- vis-reg-fit ----
@@ -159,10 +171,10 @@ ggplot(b_compare) +
 
 ## ---- vis-Bhat-lasso ----
 lasso_mb <- melt(b_lasso, varnames = c("feature", "task"),
-               value.name = "beta_hat_lasso")
+                 value.name = "beta_hat_lasso")
 
 ggplot(lasso_mb) +
-  geom_tile(aes(x = task, y = feature, fill = beta_hat)) +
+  geom_tile(aes(x = task, y = feature, fill = beta_hat_lasso)) +
   scale_fill_gradient2(midpoint = 0, high = "#90ee90", low = "#000080") +
   theme(panel.grid = element_blank())
 
@@ -174,11 +186,11 @@ reg_fit_data <- m_y %>%
 
 reg_fit_data$zero <- reg_fit_data$beta == 0
 cur_fit_data <- reg_fit_data %>%
-  filter(task %in% 20:30,
-         feature %in% 10:18) %>%
+  filter(task %in% 95:105,
+         feature %in% 1:10) %>%
   melt(measure.vars = c("beta", "beta_hat_lasso"), variable.name = "coef")
 
-## ---- vis-reg-fit ----
+## ---- vis-lasso-fit ----
 ggplot(cur_fit_data) +
   geom_point(aes(x = x, y = y, col = zero), 
              size = .3, alpha = 0.05) +
@@ -193,4 +205,3 @@ ggplot(cur_fit_data) +
         strip.background = element_blank(),
         axis.text = element_blank(),
         axis.ticks = element_blank())
-
