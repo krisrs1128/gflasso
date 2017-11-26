9ba23af4e6952a4c435c681abdd12eb8a4a9ac7d
Kris Sankaran <sankaran.kris@gmail.com>
2017-11-26 13:00:43 -0800

    roxygen-style documentation

--- R/crossval.R
+++ R/crossval.R
@@ -1,12 +1,29 @@
+#! /usr/bin/env Rscript
+
+## File description -------------------------------------------------------------
+##
+## Cross-validation functions for Graph-Fused lasso. This can't just be done
+## directly by caret since that package doesn't support multitask regression.
+##
+## author: Francisco Lima (https://github.com/monogenea), with revisions by
+## sankaran.kris@gmail.com
+## date: 11/26/2017
 
 
-# CV GFLASSO (RMSE-based, can be easily changed to R2)
-# cvIndex must be a list, each element of which carrying the indices of the rows for a particular fold (I used caret)
-# Kris, can you make the code more efficient?
-# Note that cvGFLASSO is supporting crossval
-# For now, crossval provides the average RMSE per element in the lambda*gamma grid
-
-cvGFLASSO <- function(Y, X, R, opts, cvIndex){
+#' Cross Validation for GFLasso
+#'
+#' Cross validation for GFLasso, supports crossval function.
+#'
+#' @param Y The matrix of regression responses.
+#' @param X The data matrix.
+#' @param R The matrix of (thresholded) correlations between columns of Y
+#' @param opts A potentially partially specified list of regularization and
+#' gradient descent parameters. See merge_proxgrad_opts().
+#' @param cvIndex [list] each element of which carries the indices of the rows
+#'   for a particular fold
+#' @return rmse The average RMSE per element in the lambda*gamma grid
+#' @export
+cv_gflasso <- function(Y, X, R, opts, cvIndex){
       rmse <- rep(NA, length(cvIndex))
       for(i in 1:length(cvIndex)){
             mod <- gflasso(Y = Y[-cvIndex[[i]],], X = X[-cvIndex[[i]],], R = R, opts = opts)
@@ -17,13 +34,26 @@ cvGFLASSO <- function(Y, X, R, opts, cvIndex){
       return(rmse)
 }
 
+#' Cross Validate on a prespecified grid
+#'
+#' Wrapper function for cvGFLASSO, applying it along a grid of lambda and gamma
+#' values.
+#'
+#' @param Y The matrix of regression responses.
+#' @param X The data matrix.
+#' @param R The matrix of (thresholded) correlations between columns of Y
+#' @param params The grid of lambda and gamma values to try
+#' @param cvIndex [list] each element of which carries the indices of the rows
+#'   for a particular fold
+#' @param
+#' @export
 crossval <- function(X, Y, R, params = seq(0,1,by=0.1), cvIndex){
       cvMatrix <- matrix(NA, length(params),length(params))
       dimnames(cvMatrix) <- list(params, params)
       grid <- expand.grid(lambda = params, gamma = params)
-      
+
       for(i in 1:nrow(grid)){
-            cv <- cvGFLASSO(X = X, Y = Y, R = R, opts = list(lambda = grid[i,1], gamma = grid[i,2]),
+            cv <- cv_gflasso(X = X, Y = Y, R = R, opts = list(lambda = grid[i,1], gamma = grid[i,2]),
                             cvIndex = myfolds)
             cvMatrix[as.character(grid[i,1]),as.character(grid[i,2])] <- mean(cv)
             message(paste(round((i/(nrow(grid)))*100, 2), "% completion", collapse = " "))
